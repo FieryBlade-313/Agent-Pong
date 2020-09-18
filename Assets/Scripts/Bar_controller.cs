425726551c6a2f8d445fa2e5d0f30fd7d3f6c092
FieryBlade-313 <mohdfauz.s18@iiits.in>
2020-09-19 00:05:43 +0530

    Built a basic Pong Game

---
+++ Assets/Scripts/Bar_controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bar_controller : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private float _vertDir = 0f;
+    private Rigidbody2D rb;
+    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _vertDir = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition((Vector2)transform.position + new Vector2(0, _vertDir * moveSpeed * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/ball_manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_manager : MonoBehaviour
+{
+    public float speed = 50f;
+    private Rigidbody2D rb;
+    private Vector2 prev_speed;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        //Vector2 _rand_dir = Random.insideUnitCircle.normalized;
+        Vector2 _rand_dir = Random.value > 0.5 ?  new Vector2(1,0.5f): new Vector2(1,-0.5f);
+        rb.velocity = _rand_dir * speed;
+        prev_speed = rb.velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Vector2 _nrm = other.contacts[0].normal;
+        Vector2 _ref_dir = Vector2.Reflect(prev_speed, _nrm).normalized;
+        rb.velocity = _ref_dir * speed;
+        prev_speed = rb.velocity;
+    }
+}
